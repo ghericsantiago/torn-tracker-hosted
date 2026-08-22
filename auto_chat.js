1a768acbe555a2e9a25beae697eb44203b5643de
gheric <ghericsantiago@gmail.com>
2026-08-22 19:56:18 +0800

    test

---
+++ auto_chat.js
@@ -0,0 +1,16 @@
+const timeout = setTimeout(() => {
+    const value = `💰 BUYING ITEMS 💰<br>🌸 Flowers 💯% MV<br>🧸 Plushies 💯% MV<br>🍫 Candy ⚡ Energy + More<br>➡️<a href="https://itrade.devs.surf/trade/">Prices</a><br>➡️<a href="//www.torn.com/trade.php#step=start&userID=3308452">Trade</a><br>🟢 <a href="//www.torn.com/bazaar.php?userId=3308452">Bazaar Open</a>`;
+    const textarea = document.querySelector('#chatRoot textarea');
+    const setter = Object.getOwnPropertyDescriptor(
+        HTMLTextAreaElement.prototype,
+        "value"
+    ).set;
+    setter.call(textarea, value);
+    textarea.dispatchEvent(new Event("input", {
+        bubbles: true
+    }));
+    textarea.dispatchEvent(new Event("change", {
+        bubbles: true
+    }));
+    document.querySelectorAll('#chatRoot button')[1].click();
+}, 1000 * 60);
